fafd18193e59647faa5c3280d519c90cd07dca13
bhanuandrajula <bhanua1985@gmail.com>
2014-11-23 11:05:30 -0500

    added pages names

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,17 @@
 class PagesController < ApplicationController
   def home
+    @title = "Home"
   end
 
   def services
+    @title = "Services"
   end
 
   def gallery
+    @title = "Gallery"
   end
 
   def contact
+    @title = "Contact"
   end
 end
